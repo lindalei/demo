59d05c209a8e4f39be2aba83a9e919a6b382f41f
leisurega <1019362617@qq.com>
2019-02-21 23:39:03 +0800

    reduce, IntStream max/sum/mapToObj/boxed

--- src/main/java/com/linda/demo/detail/Lamda.java
+++ src/main/java/com/linda/demo/detail/Lamda.java
@@ -2,8 +2,10 @@ package com.linda.demo.detail;
 
 import java.util.*;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 public class Lamda {
@@ -21,6 +23,7 @@ public class Lamda {
     public static void main(String[] args) {
         List<Apple> apples = Arrays.asList(
                 new Apple("green", 340),
+                new Apple("green", 440),
                 new Apple("pink", 100),
                 new Apple("yellow", 450),
                 new Apple("purple", 200),
@@ -53,9 +56,15 @@ public class Lamda {
 
         List<Integer> numbers1 = Arrays.asList(1,2,3);
         List<Integer> numbers2 = Arrays.asList(2,4);
-//        Stream<Stream<Integer[]>> biNumbers = numbers1.stream()
-//                .map(n1->numbers2.stream()
-//                        .map(n2->new Integer[]{n1,n2}));
+        Stream<Stream<Integer[]>> biNumbers0 = numbers1.stream()
+                .map(n1->numbers2.stream()
+                        .map(n2->new Integer[]{n1,n2}));
+
+        Stream<Integer[]> biNumbers2 = numbers1.stream()
+                .flatMap((n1->numbers2.stream()
+                        .map(n2->new Integer[]{n1,n2})));
+
+        biNumbers2.forEach(t->System.out.println(t[0]+"-"+t[1]));
 
         //mapping
         System.out.println(numbers1.stream()
@@ -84,6 +93,88 @@ public class Lamda {
         System.out.println(apples.stream()
                 .anyMatch(t->t.getWeight()>300));
 
+        //reduce sums with initial value
+        System.out.println(apples.stream()
+                .map(Apple::getWeight)
+        .reduce(0,Integer::sum)); //有装箱成本
 
+        //reduce sums with initial value
+        System.out.println(apples.stream()
+                .mapToInt(Apple::getWeight)
+                .sum()); //没有装箱成本,sum有默认值0，而max没有，直接调用max返回OptionalInt,需要再调用orElse
+
+        //reduce sum no initial value
+        System.out.println(apples.stream()
+                .map(Apple::getWeight)
+                .reduce(Integer::sum).orElse(300));
+
+        //max
+        System.out.println(apples.stream()
+                .map(Apple::getWeight)
+                .reduce(Integer::max).orElse(100));
+
+        //ntStream max, from Stream<>to IntStream: mapToInt()
+        System.out.println(apples.stream()
+                .mapToInt(Apple::getWeight)
+                .max().orElse(3));
+
+        //count
+        System.out.println(apples.stream()
+      .count());
+
+        //collect color string: to list
+        System.out.println(apples.stream()
+                .map(Apple::getColor)
+        .distinct()
+        .sorted()
+        .collect(toList()));
+
+        //collect color string: by lamda
+        System.out.println(apples.stream()
+                .map(Apple::getColor)
+                .distinct()
+                .sorted()
+                .reduce("",(c1,c2)->c1+" "+c2));
+
+        //collect color string: joining
+        System.out.println(apples.stream()
+                .map(Apple::getColor)
+                .distinct()
+                .sorted()
+                .collect(joining()));
+
+        //IntStream count
+        System.out.println(
+                IntStream.rangeClosed(1,200).count());
+
+        //from IntStream to Steam<>, boxed()
+        System.out.println(
+                IntStream.rangeClosed(1,200).boxed().collect(toList()));
+
+        //勾股数
+        Stream<int[]> sqrtNumbers=
+                IntStream.rangeClosed(1,100)
+                        .boxed().flatMap(a->
+                        IntStream.rangeClosed(a,100)
+        .filter(b->Math.sqrt(a*a+b*b)%1==0)
+        .boxed()
+        .map(b-> new int[]{a,b,(int)Math.sqrt(a*a+b*b)}));
+
+        Stream<int[]> sqrtNumbers2=
+                IntStream.rangeClosed(1,100)
+                        .boxed().flatMap(a->
+                        IntStream.rangeClosed(a,100)
+                                .filter(b->Math.sqrt(a*a+b*b)%1==0)
+                                .mapToObj(b-> new int[]{a,b,(int)Math.sqrt(a*a+b*b)}));
+
+        //先生成数组再过滤
+        Stream<int[]> sqrtNumbers3=
+                IntStream.rangeClosed(1,100)
+                        .boxed().flatMap(a->
+                        IntStream.rangeClosed(a,100)
+                                .mapToObj(b-> new int[]{a,b,(int)Math.sqrt(a*a+b*b)})
+                .filter(t->t[2]%1==0));
+
+        sqrtNumbers3.forEach(t->System.out.println(t[0]+", "+t[1]+", "+t[2]));
     }
 }
